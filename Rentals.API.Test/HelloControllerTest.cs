2006b1de80acc341a49a0851f812336576d12679
srmah01 <srmah01@gmail.com>
2017-07-17 17:36:55 +0100

    Add test of hello controller.

---
+++ Rentals.API.Test/HelloControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RentalsAPI.Controllers;
+using System.Web.Http.Results;
+
+namespace Rentals.API.Test
+{
+    [TestClass]
+    public class HelloControllerTest
+    {
+        [TestMethod]
+        public void GetShouldReturnResponse()
+        {
+            var controller = new HelloController();
+
+            var response = controller.Get() as OkNegotiatedContentResult<HelloResponse>; ;
+
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Content.Data.Contains("Hello there!!"));
+        }
+    }
+}
